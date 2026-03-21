0031cc775ddcede10d3470f22c2f95f038c712a1
Roberto <robertofilluelo@hotmail.com>
2026-03-21 19:05:40 -0300

    disable auth temporarily

--- auth.ts
+++ auth.ts
@@ -1,19 +1,6 @@
 import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-
-const prisma = new PrismaClient()
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    Google({
-      clientId: process.env.AUTH_GOOGLE_ID!,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
-    }),
-  ],
-  secret: process.env.AUTH_SECRET,
-  trustHost: true, // 👈 IMPORTANTE
+  providers: [],
+  trustHost: true,
 })
-  
